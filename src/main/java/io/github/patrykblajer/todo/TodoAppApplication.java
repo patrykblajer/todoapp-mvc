fe0487bb7166fc3935d97b81b56b14f4b1feff9e
Patryk <patrykblajer@gmail.com>
2021-11-07 11:44:07 +0100

    add jasypt, encrypt api

--- src/main/java/io/github/patrykblajer/todo/TodoAppApplication.java
+++ src/main/java/io/github/patrykblajer/todo/TodoAppApplication.java
@@ -1,11 +1,15 @@
 package io.github.patrykblajer.todo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @SpringBootApplication
+@EnableEncryptableProperties
+@PropertySource(name = "EncryptedProperties", value = "classpath:encrypted.properties")
 public class TodoAppApplication {
 
     public static void main(String[] args) {
